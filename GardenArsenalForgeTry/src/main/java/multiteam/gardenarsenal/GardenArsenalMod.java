58dcd2ba82c5d57bc5a1e0915b631c18332f8370
LazTheArtist <zoltan12174@citromail.hu>
2021-02-20 14:27:19 +0100

    skins are repetative as hell

--- GardenArsenalForgeTry/src/main/java/multiteam/gardenarsenal/GardenArsenalMod.java
+++ GardenArsenalForgeTry/src/main/java/multiteam/gardenarsenal/GardenArsenalMod.java
@@ -70,6 +70,34 @@ public class GardenArsenalMod {
                         return 0;
                     case "camo_desert":
                         return 1;
+                    case "camo_end":
+                        return 2;
+                    case "camo_forest":
+                        return 3;
+                    case "camo_frost":
+                        return 4;
+                    case "camo_nether":
+                        return 5;
+                    case "metallic_gold":
+                        return 6;
+                    case "metallic_iron":
+                        return 7;
+                    case "metallic_netherite":
+                        return 8;
+                    case "seasonal_christmas":
+                        return 9;
+                    case "seasonal_halloween":
+                        return 10;
+                    case "special_aquatic":
+                        return 11;
+                    case "special_neon":
+                        return 12;
+                    case "teams_mcabnormals":
+                        return 13;
+                    case "teams_multiteam":
+                        return 14;
+                    case "teams_vampirestudios":
+                        return 15;
                         // continue
                 }
             }
