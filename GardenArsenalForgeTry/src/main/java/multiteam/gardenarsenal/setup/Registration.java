9ec1ff3ba6452984ff7a7dde1d6af1c6ce0a4605
LazTheArtist <zoltan12174@citromail.hu>
2021-02-22 15:24:10 +0100

    villagers? more like, "nah i cant code"

--- GardenArsenalForgeTry/src/main/java/multiteam/gardenarsenal/setup/Registration.java
+++ GardenArsenalForgeTry/src/main/java/multiteam/gardenarsenal/setup/Registration.java
@@ -1,10 +1,14 @@
 package multiteam.gardenarsenal.setup;
 
+import com.google.common.collect.ImmutableSet;
 import multiteam.gardenarsenal.GardenArsenalMod;
 import net.minecraft.block.Block;
 import net.minecraft.entity.merchant.villager.VillagerProfession;
 import net.minecraft.item.Item;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.village.PointOfInterestType;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,6 +17,7 @@ public class Registration {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, GardenArsenalMod.MOD_ID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GardenArsenalMod.MOD_ID);
     public static final DeferredRegister<VillagerProfession> VILLAGER_PROFESSIONS = DeferredRegister.create(ForgeRegistries.PROFESSIONS, GardenArsenalMod.MOD_ID);
+    // WHAT IS HAPPENING   public static final RegistryObject<VillagerProfession> SOLDEIROFFICER = VILLAGER_PROFESSIONS.register("garsen_soldier_commandr",() -> new VillagerProfession("garsen_soldier_commandr", new PointOfInterestType("hunting_table", ImmutableSet.of(), ImmutableSet.of(), 5, 40), ImmutableSet.of(ModBlocks.GARDEN_SOLDIER_COMMANDER_POI), SoundEvents.ENTITY_VILLAGER_WORK_BUTCHER));
 
 
     public static void register() {
@@ -20,6 +25,7 @@ public class Registration {
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
         VILLAGER_PROFESSIONS.register(modEventBus);
+
 
         ModBlocks.register();
         ModItems.register();
--- GardenArsenalForgeTry/src/main/java/multiteam/gardenarsenal/setup/entitys/villager/VillagerProfessions.java
+++ GardenArsenalForgeTry/src/main/java/multiteam/gardenarsenal/setup/entitys/villager/VillagerProfessions.java
@@ -22,9 +22,16 @@ public class VillagerProfessions {
     /**
     public static final DeferredRegister<VillagerProfession> PROFESSIONS = DeferredRegister.create(ForgeRegistries.PROFESSIONS, GardenArsenalMod.MOD_ID);
 
-    public static final RegistryObject<VillagerProfession> SOLDEIROFFICER = PROFESSIONS.register("hunter",() -> new VillagerProfession("hunter",new PointOfInterestType("hunting_table",ImmutableSet.of(ModBlocks.GARDEN_SOLDIER_COMMANDER_POI.get().getDefaultState()), 5, 40), ImmutableSet.of(ModBlocks.HUNTING_TABLE.get()),SoundEvents.ENTITY_VILLAGER_WORK_BUTCHER));
+    public static final RegistryObject<VillagerProfession> SOLDEIROFFICER = PROFESSIONS.register("garsen_soldier_commandr",() -> new VillagerProfession("garsen_soldier_commandr", new PointOfInterestType("hunting_table",ImmutableSet.of(ModBlocks.GARDEN_SOLDIER_COMMANDER_POI.get().getDefaultState()), 5, 40), ImmutableSet.of(ModBlocks.HUNTING_TABLE.get()),SoundEvents.ENTITY_VILLAGER_WORK_BUTCHER));
 
      send help ;-;
+
+     or this?
+
+     private static final ImmutableSet<Item> HUNTER_ITEMS = ImmutableSet.of();
+     public static final RegistryObject<VillagerProfession> HUNTER = VILLAGER_PROFESSIONS.register("garden_soldier_commander", () -> new VillagerProfession("garden_soldier_commander", new PointOfInterestType("garden_soldier_commander_poi", ImmutableSet.of(ModBlocks.GARDEN_SOLDIER_COMMANDER_POI.get().getDefaultState()), 5, 40), HUNTER_ITEMS, ImmutableSet.of(ModBlocks.GARDEN_SOLDIER_COMMANDER_POI.get()),SoundEvents.ENTITY_VILLAGER_WORK_BUTCHER));
+
+     nah, still nothing ;-;
      **/
 
     @SubscribeEvent
