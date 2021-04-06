cd1d04743156fcd58f928ca5d1caa032ff7f2f44
Cat Core <arthur.bambou@gmail.com>
2021-04-06 18:59:24 +0200

    Mixins are magic
    Villager houses now generate in villages :)

---
+++ common/src/main/java/multiteam/gardenarsenal/mixins/StructureTemplatePoolAccessor.java
@@ -0,0 +1,19 @@
+package multiteam.gardenarsenal.mixins;
+
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.world.level.levelgen.feature.structures.StructurePoolElement;
+import net.minecraft.world.level.levelgen.feature.structures.StructureTemplatePool;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+
+@Mixin(StructureTemplatePool.class)
+public interface StructureTemplatePoolAccessor {
+
+    @Accessor("rawTemplates")
+    List<Pair<StructurePoolElement, Integer>> getRawTemplates();
+
+    @Accessor("templates")
+    List<StructurePoolElement> getTemplates();
+}
---
+++ common/src/main/java/multiteam/gardenarsenal/utils/ModifiableStructurePool.java
@@ -0,0 +1,35 @@
+package multiteam.gardenarsenal.utils;
+
+import com.mojang.datafixers.util.Pair;
+import multiteam.gardenarsenal.mixins.StructureTemplatePoolAccessor;
+import net.minecraft.world.level.levelgen.feature.structures.StructurePoolElement;
+import net.minecraft.world.level.levelgen.feature.structures.StructureTemplatePool;
+
+import java.util.List;
+
+public class ModifiableStructurePool {
+    private final StructureTemplatePool pool;
+
+    public ModifiableStructurePool(StructureTemplatePool pool) {
+        this.pool = pool;
+    }
+
+    public void addStructurePoolElement(StructurePoolElement element) {
+        addStructurePoolElement(element, 1);
+    }
+
+    public void addStructurePoolElement(StructurePoolElement element, int weight) {
+        for (int i = 0; i < weight; i++) {
+            ((StructureTemplatePoolAccessor)pool).getTemplates().add(element);
+        }
+        ((StructureTemplatePoolAccessor)pool).getRawTemplates().add(Pair.of(element, weight));
+    }
+
+    public List<StructurePoolElement> getElements() {
+        return ((StructureTemplatePoolAccessor)pool).getTemplates();
+    }
+
+    public StructureTemplatePool getStructurePool() {
+        return pool;
+    }
+}
