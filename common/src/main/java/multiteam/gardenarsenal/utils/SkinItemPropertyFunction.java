8d7a08a757d502ebab12cc5e8f7a0d630cc98fc7
Cat Core <arthur.koalas@free.fr>
2021-09-24 00:51:33 +0200

    not needed anymore

--- common/src/main/java/multiteam/gardenarsenal/utils/SkinItemPropertyFunction.java
+++ common/src/main/java/multiteam/gardenarsenal/utils/SkinItemPropertyFunction.java
@@ -17,8 +17,4 @@ public class SkinItemPropertyFunction implements ClampedItemPropertyFunction {
         Skins Skin = skinName.isEmpty() ? Skins.Default : Skins.valueOf(skinName);
         return Skin.ordinal() >= 15 ? (float) ((Skin.ordinal() + 1) / 100.0) : (float) (Skin.ordinal() / 100.0);
     }
-
-    public float m_142187_(ItemStack arg, @Nullable ClientLevel arg2, @Nullable LivingEntity arg3, int i) {
-        return this.unclampedCall(arg, arg2, arg3, i);
-    }
 }
