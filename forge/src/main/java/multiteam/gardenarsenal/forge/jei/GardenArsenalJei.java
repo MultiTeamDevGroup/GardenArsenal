f223aa5f5eb06cd9a4ed735a6c13c39142a548a4
Cat Core <arthur.koalas@free.fr>
2021-11-22 01:24:52 +0100

    JEI Plugin

---
+++ forge/src/main/java/multiteam/gardenarsenal/forge/jei/GardenArsenalJei.java
@@ -0,0 +1,22 @@
+package multiteam.gardenarsenal.forge.jei;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeRegistration;
+import multiteam.gardenarsenal.GardenArsenal;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+@JeiPlugin
+public class GardenArsenalJei implements IModPlugin {
+    @Override
+    public @NotNull ResourceLocation getPluginUid() {
+        return new ResourceLocation(GardenArsenal.MOD_ID, "default");
+    }
+
+    @Override
+    public void registerRecipes(@NotNull IRecipeRegistration registration) {
+        IModPlugin.super.registerRecipes(registration);
+        registration.addRecipes(RecipeHelper.createSkinRecipes(), new ResourceLocation("minecraft:smithing"));
+    }
+}
---
+++ forge/src/main/java/multiteam/gardenarsenal/forge/jei/RecipeHelper.java
@@ -0,0 +1,54 @@
+package multiteam.gardenarsenal.forge.jei;
+
+import dev.architectury.registry.registries.RegistrySupplier;
+import multiteam.gardenarsenal.GardenArsenal;
+import multiteam.gardenarsenal.utils.Skins;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.UpgradeRecipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipeHelper {
+
+    public static List<Object> createSkinRecipes() {
+        List<Object> list = new ArrayList<>();
+
+        int i = 0;
+        for (Skins skin : Skins.values()) {
+            i++;
+
+            int j = 0;
+            for (RegistrySupplier<Item> item : skin.getWeapons()) {
+                j++;
+                Item weapon = item.getOrNull();
+                ItemStack result = new ItemStack(weapon);
+                result.getOrCreateTag().putString("skinType", skin.name());
+                list.add(new UpgradeRecipe(
+                        new ResourceLocation(GardenArsenal.MOD_ID, "skin_" + i + "_" + j),
+                        getWeaponVariants(weapon),
+                        Ingredient.of(new ItemStack(skin.getItem().getOrNull())),
+                        result
+                ));
+            }
+        }
+
+        return list;
+    }
+
+    private static Ingredient getWeaponVariants(Item weapon) {
+        List<ItemStack> list = new ArrayList<>();
+
+        for (Skins skin : Skins.values()) {
+            ItemStack stack = new ItemStack(weapon);
+            stack.getOrCreateTag().putString("skinType", skin.name());
+
+            list.add(stack);
+        }
+
+        return Ingredient.of(list.stream());
+    }
+}
