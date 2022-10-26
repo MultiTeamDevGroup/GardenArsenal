efbc52270238eb47914e3d5d73c912c9eff05abf
Cat Core <arthur.koalas@free.fr>
2022-10-26 17:59:08 +0200

    update dependencies

--- common/src/main/java/multiteam/gardenarsenal/mixin/GuiMixin.java
+++ common/src/main/java/multiteam/gardenarsenal/mixin/GuiMixin.java
@@ -2,25 +2,17 @@ package multiteam.gardenarsenal.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
-import multiteam.gardenarsenal.GardenArsenal;
 import multiteam.gardenarsenal.accessor.GuiAccessor;
-import multiteam.gardenarsenal.registries.GardenArsenalItems;
 import multiteam.gardenarsenal.utils.Utils;
-import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Gui.class)
 public abstract class GuiMixin extends GuiComponent implements GuiAccessor {
