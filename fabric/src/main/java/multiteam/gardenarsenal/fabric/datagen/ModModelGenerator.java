69781bdfd4b4ece86de7b97241a6a3c0f8a7cda3
Cat Core <arthur.koalas@free.fr>
2025-10-05 13:41:20 +0200

    Add some block models to Data gen

--- fabric/src/main/java/multiteam/gardenarsenal/fabric/datagen/ModModelGenerator.java
+++ fabric/src/main/java/multiteam/gardenarsenal/fabric/datagen/ModModelGenerator.java
@@ -2,16 +2,22 @@ package multiteam.gardenarsenal.fabric.datagen;
 
 import multiteam.gardenarsenal.GardenArsenal;
 import multiteam.gardenarsenal.items.SkinCardItem;
+import multiteam.gardenarsenal.registries.GardenArsenalBlocks;
 import multiteam.gardenarsenal.registries.GardenArsenalItems;
 import multiteam.gardenarsenal.utils.Skins;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.models.BlockModelGenerators;
 import net.minecraft.data.models.ItemModelGenerators;
+import net.minecraft.data.models.blockstates.MultiVariantGenerator;
+import net.minecraft.data.models.blockstates.PropertyDispatch;
+import net.minecraft.data.models.blockstates.Variant;
+import net.minecraft.data.models.blockstates.VariantProperties;
 import net.minecraft.data.models.model.ModelLocationUtils;
 import net.minecraft.data.models.model.ModelTemplates;
 import net.minecraft.data.models.model.TextureMapping;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
 public class ModModelGenerator extends FabricModelProvider {
     public ModModelGenerator(FabricDataGenerator dataGenerator) {
@@ -20,7 +26,20 @@ public class ModModelGenerator extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockModelGenerators blockStateModelGenerator) {
+        blockStateModelGenerator.createTrivialCube(GardenArsenalBlocks.MACHINE_BLOCK.get());
 
+        blockStateModelGenerator.createSimpleFlatItemModel(GardenArsenalItems.TRAP_CAKE.get());
+        blockStateModelGenerator.blockStateOutput.accept(MultiVariantGenerator.multiVariant(GardenArsenalBlocks.TRAP_CAKE.get())
+                .with(PropertyDispatch.property(BlockStateProperties.BITES)
+                        .select(0, Variant.variant().with(VariantProperties.MODEL, ModelLocationUtils.getModelLocation(GardenArsenalBlocks.TRAP_CAKE.get(), "/uneaten")))
+                        .select(1, Variant.variant().with(VariantProperties.MODEL, ModelLocationUtils.getModelLocation(GardenArsenalBlocks.TRAP_CAKE.get(), "/slice1")))
+                        .select(2, Variant.variant().with(VariantProperties.MODEL, ModelLocationUtils.getModelLocation(GardenArsenalBlocks.TRAP_CAKE.get(), "/slice2")))
+                        .select(3, Variant.variant().with(VariantProperties.MODEL, ModelLocationUtils.getModelLocation(GardenArsenalBlocks.TRAP_CAKE.get(), "/slice3")))
+                        .select(4, Variant.variant().with(VariantProperties.MODEL, ModelLocationUtils.getModelLocation(GardenArsenalBlocks.TRAP_CAKE.get(), "/slice4")))
+                        .select(5, Variant.variant().with(VariantProperties.MODEL, ModelLocationUtils.getModelLocation(GardenArsenalBlocks.TRAP_CAKE.get(), "/slice5")))
+                        .select(6, Variant.variant().with(VariantProperties.MODEL, ModelLocationUtils.getModelLocation(GardenArsenalBlocks.TRAP_CAKE.get(), "/slice6")))
+                )
+        );
     }
 
     @Override
